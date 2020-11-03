e5d4422666d8a1a3f7055714895920a1954a151f
小七 <liulangdelong@gmail.com>
2020-11-03 14:50:30 +0800

    工厂模式：扇形分发群发

--- rabbitmq-api-demo/src/main/java/m3/producer.java
+++ rabbitmq-api-demo/src/main/java/m3/producer.java
@@ -5,6 +5,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -22,11 +23,21 @@ public class producer {
         f.setPassword("admin");
         Channel c = f.newConnection().createChannel();
 
-        // 定义交换机    模拟发送日志
+        // 定义扇形扇出的交换机    模拟发送日志
 //        c.exchangeDeclare("log", "fanout");
         c.exchangeDeclare("log", BuiltinExchangeType.FANOUT);
 
         // 向交换机发送消息
+        while (true) {
+            System.out.println("输入信息：");
+            String msg = new Scanner(System.in).nextLine();
+            /*
+            log: 交换机的名字(简单模式和工作模式时是“”， 默认是使用的direct交换机)
+            routingKey: 群发模式下，对付fanout交换机，这个参数无效；它只会向事先绑定好的消费者发送消息
+            props:其他属性
 
+             */
+            c.basicPublish("log", "", null, msg.getBytes());
+        }
     }
 }
