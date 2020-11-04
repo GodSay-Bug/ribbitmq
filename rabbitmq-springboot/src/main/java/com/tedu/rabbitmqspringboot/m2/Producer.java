7ddfca41affed734a7dcfe967a0eb7859001edee
小七 <liulangdelong@gmail.com>
2020-11-04 14:47:41 +0800

    工作模式的非持久设置

--- rabbitmq-springboot/src/main/java/com/tedu/rabbitmqspringboot/m2/Producer.java
+++ rabbitmq-springboot/src/main/java/com/tedu/rabbitmqspringboot/m2/Producer.java
@@ -1,6 +1,7 @@
 package com.tedu.rabbitmqspringboot.m2;
 
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +22,21 @@ public class Producer {
         while (true) {
             System.out.println("输入");
             String msg = new Scanner(System.in).nextLine();
+            // 默认是持久消息
             t.convertAndSend("task_Queue", msg);
+
+            // 如果要发送非持久消息，需要一个消息预处理对象先修改消息属性，然后再进行发送
+            /*t.convertAndSend("task_Queue", (Object) msg, new MessagePostProcessor() {
+                @Override
+                public Message postProcessMessage(Message message) throws AmqpException {
+                    MessageProperties messages = message.getMessageProperties();// 调整为非持久
+                    messages.setDeliveryMode(MessageDeliveryMode.NON_PERSISTENT);
+                    return message;
+
+                }
+            });*/
+
+
         }
     }
 }
